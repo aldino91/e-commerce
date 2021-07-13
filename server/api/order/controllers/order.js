ebca08a9ad99e4db072c7937375145be205921ad
aldino91 <aldobrunet@icloud.com>
2021-07-13 20:17:13 +0200

    init

---
+++ server/api/order/controllers/order.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const stripe = require("stripe")(
+  "sk_test_51IfWJFK9xIjW9yez4ZF9yViodVkfl1xwNuMjGC5MuCcTNx7YuyucbVhwgxgo598tEj1KDb1Vjhcj8JI8hDHxgCbv00EI8I5Lju"
+);
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async create(ctx) {
+    const { token, products, idUser, addressShipping } = ctx.request.body;
+    let totalPayment = 0;
+    products.forEach((product) => {
+      totalPayment = totalPayment + product.price;
+    });
+
+    const charge = await stripe.charges.create({
+      amount: totalPayment * 100,
+      currency: "eur",
+      source: token.id,
+      description: `ID Usuario: ${idUser}`,
+    });
+
+    const createOrder = [];
+    for await (const product of products) {
+      const data = {
+        lavori: product.id,
+        users_permissions_user: idUser,
+        totalPayment,
+        idPayment: charge.id,
+        addressShipping,
+      };
+      const validData = await strapi.entityValidator.validateEntityCreation(
+        strapi.models.order,
+        data
+      );
+      const entry = await strapi.query("order").create(validData);
+      createOrder.push(entry);
+    }
+    return createOrder;
+  },
+};
---
+++ server/api/order/models/order.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {};
+("use strict");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {};
